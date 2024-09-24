3942751d787e4384169e63506dbb25b399f346e6
magomed <magomedyasulov8@gmail.com>
2024-09-24 19:50:30 +0300

    Добавьте файлы проекта.

---
+++ PasswordManager.Frontend/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace PasswordManager.Frontend.ViewModels
+{
+    public class ViewModelBase : ObservableObject
+    {
+    }
+}
---
+++ PasswordManager.Frontend/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace PasswordManager.Frontend.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
