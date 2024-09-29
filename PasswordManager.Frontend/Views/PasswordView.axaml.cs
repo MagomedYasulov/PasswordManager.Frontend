bd74ecb717e312eb3a984ec6110a89660576a213
magomed <magomedyasulov8@gmail.com>
2024-09-30 00:15:59 +0300

    feat: макет пароля

---
+++ PasswordManager.Frontend/Views/PasswordView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace PasswordManager.Frontend;
+
+public partial class PasswordView : UserControl
+{
+    public PasswordView()
+    {
+        InitializeComponent();
+    }
+}
