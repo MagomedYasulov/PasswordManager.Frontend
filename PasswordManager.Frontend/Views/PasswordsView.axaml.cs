95ef45d94e99b4e7237214e60b8ca1fc0dcd5ce8
magomed <magomedyasulov8@gmail.com>
2024-09-29 22:19:05 +0300

    refactor: вынес содержимое основного окна в отделеный userControl

---
+++ PasswordManager.Frontend/Views/PasswordsView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace PasswordManager.Frontend;
+
+public partial class PasswordsView : UserControl
+{
+    public PasswordsView()
+    {
+        InitializeComponent();
+    }
+}
